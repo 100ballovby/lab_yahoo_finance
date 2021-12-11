38eb06b08ad5ae937e80c5f4833dddaddb5e8e55
demidraksin <greatraksin@icloud.com>
2021-12-11 18:05:34 +0300

    Adding constants and requirements🧠

--- functions.py
+++ functions.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from datetime import datetime
 
+KEY = os.environ.get('API_KEY')
+HOST = os.environ.get('API_HOST')
+
+symbol_string = ''
+data = {}
+
