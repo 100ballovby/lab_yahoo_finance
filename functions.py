02ada8d94208446fe19e89440add0265b5c5a7e3
demidraksin <greatraksin@icloud.com>
2021-12-11 17:49:11 +0300

    Adding functions 🐍

---
+++ functions.py
@@ -0,0 +1,9 @@
+import os
+import json
+import requests
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from datetime import datetime
+
